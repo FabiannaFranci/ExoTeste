6fc010c34b0aa5f75634136a5e7090a8b496efb7
Fabianna Franci <110000787+FabiannaFranci@users.noreply.github.com>
2023-02-16 16:36:13 -0300

    Teste do Login

---
+++ Exo.WebApi/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using Exo.WebApi.Interface;
+using Exo.WebApi.Models;
+using Exo.WebApi.Repositories;
+using Exo.WebApi.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Exo.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IUserRepository _IUserRepository;
+        
+
+        public LoginController(IUserRepository IUserRepository) 
+        {
+            _IUserRepository = IUserRepository;
+        }
+
+
+   
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel login)
+        {
+            try
+            {
+                User foundUser = _IUserRepository.Login(login.Email, login.Senha); 
+                if (foundUser == null)
+                { 
+                    return Unauthorized(new { msg = "Usuário não autorizado"});
+                }
+                var myClaims = new[]
+                {
+                    new Claim (JwtRegisteredClaimNames.Email, foundUser.Email),
+                    new Claim (JwtRegisteredClaimNames.Jti, foundUser.Id.ToString()),
+                    new Claim (ClaimTypes.Role, foundUser.Tipo)
+                };
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("project-key-authentication"));
+
+                var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var myToken = new JwtSecurityToken(
+                    issuer: "Exo.WebApi",
+                    audience: "Exo.WebApi",
+                    claims: myClaims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: credentials
+
+                    ); ;
+                return Ok(
+                    new { token = new JwtSecurityTokenHandler().WriteToken(myToken) }
+                    );
+
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e);
+            }
+        }
+    }
+    
+}
